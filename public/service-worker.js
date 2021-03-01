ffd8ebd7ce84408857b0a417dee3cabe4318c68b
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-03-01 18:03:34 -0500

    tags added to html and service worker adjusted

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,75 +1,69 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/manifest.webmanifest",
-    "/style.css",
-    "index.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+  "/",
+  "/index.html",
+  "/assets/css/styles.css",
+  "/assets/js/index.js",
+  "/assets/js/db.js",
+  "/assets/images/icons/icon-192x192.png",
+  "/assets/images/icons/icon-512x512.png"
 ];
 
-//install
-self.addEventListener("install", function (evt) {
-    // pre cache all static assets
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {return cache.addAll(FILES_TO_CACHE)})
-    );
-  
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
-    self.skipWaiting();
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
 });
 
-//activate
-self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
-//fetch
-self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
           return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
+            .then((response) => {
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(evt.request, response.clone());
               }
-  
+
               return response;
             })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
+            .catch(() => {
+              
               return cache.match(evt.request);
             });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
-    }
-  
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
-      })
+        })
+        .catch((err) => console.log(err))
     );
+    return;
+  }
+
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
 });
